67e0587d25abe927c73f4e87468897ffb5d6f4ad
Spax <imstuff@spax.zone>
2026-03-21 22:35:50 -0700

    quick embed fix

--- src/worker.js
+++ src/worker.js
@@ -50,6 +50,10 @@ async function fetchLang(env, request) {
   const dev = /^localhost:\d+$/.test(domain);
   const subdomain = !dev && domain !== url.host ? url.hostname.slice(0, url.hostname.lastIndexOf(env.DOMAIN) - 1) : "";
   
+  if (/^\/S(\d+(\.SSx?\d+)?|x\d+)$/i.test(url.pathname)) {
+    return await env.ASSETS.fetch(`${url.protocol}//${domain}/${subdomain}`);
+  }
+  
   if (path === "en" || subdomain === "en") {
     return new Response(null, {
       "status": 301,
@@ -68,7 +72,7 @@ async function fetchLang(env, request) {
     });
   }
   
-  if (/\w{2}/.test(path) && path !== subdomain) {
+  if (/[a-z]{2}/i.test(path) && path !== subdomain) {
     return new Response(null, {
       "status": 301,
       "headers": {
@@ -89,10 +93,7 @@ export default {
     const url = new URL(request.url);
     if (/^\/S(\d+(\.SSx?\d+)?|x\d+)$/i.test(url.pathname)) {
       if (embedFetchingApps.some(app=>ua.includes(app))) {
-        if (url.pathname.split(".").length > 1) {
-          return await env.ASSETS.fetch(request);
-        }
-        const doc = await fetchLang(env, request).then(response => response.html());
+        const doc = await fetchLang(env, request).then(response => response.text());
         const { document } = parseHTML(doc);
         const linkedElement = document.getElementById(url.pathname.slice(1));
         if (linkedElement) {
