81b16caf95a00d988150c131c3089d61f382fb4b
Spax <imstuff@spax.zone>
2026-04-26 02:08:13 -0700

    fix/adjust parts of soup.py

--- src/soup.py
+++ src/soup.py
@@ -8,7 +8,7 @@ import sys
 # 1. soup the toc title text to be accurate
 # 2. maybe figure out the href title text for external hrefs?
 # 3. add alt text to images???
-# 4. fix "refer to caption" localization!!!
+# 4. fix 'refer to caption' localization!!!
 
 # MUST ADD FOR NEW LANGS
 lang_flags = {'en': '🇺🇸 English', 'de': '🇩🇪 Deutsch', 'fr': '🇫🇷 Français'}
@@ -19,39 +19,39 @@ lang_flags = {'en': '🇺🇸 English', 'de': '🇩🇪 Deutsch', 'fr': '🇫�
 
 # check if a code is used
 if len(sys.argv) < 2:
-    print("MISSING LANGUAGE CODE. Usage: python soup.py <language code>") 
-    sys.exit(1) 
+    print('MISSING LANGUAGE CODE. Usage: python soup.py <language code>')
+    sys.exit(1)
 
 # check if too many codes are used for some reason
 #if len(sys.argv) > 2:
-#    print("TOO MANY LANGUAGE CODES. Usage: python soup.py <language code>") 
-#    sys.exit(1) 
+#    print('TOO MANY LANGUAGE CODES. Usage: python soup.py <language code>')
+#    sys.exit(1)
 
 # build spice cabinet to make soup for each language
 # file path relative to main. example: trans/de/spices_de.csv
 language = sys.argv[1]
 domain = sys.argv[2]
-spice_name = "spices_" + language + ".csv"
-cabinet_file = os.path.join("trans",language,spice_name)
-print("Language used: ",language)
+spice_name = 'spices_' + language + '.csv'
+cabinet_file = os.path.join('trans',language,spice_name)
+print('Language used: ',language)
 
 # choose html and cover for language
 # en carve out for mandatory index.html and no subdomain
-if language == "en":
-    html_loc_name = "index.html"
-    og_url_tag = "https://" + domain
+if language == 'en':
+    html_loc_name = 'index.html'
+    og_url_tag = 'https://' + domain
 else:
-    html_loc_name = language + ".html"
-    og_url_tag = "https://" + language + "." + domain
-html_file = os.path.join("export",html_loc_name)
-cover_tag = "/img/cover_" + language + ".png"
+    html_loc_name = language + '.html'
+    og_url_tag = 'https://' + language + '.' + domain
+html_file = os.path.join('export',html_loc_name)
+cover_tag = '/img/cover_' + language + '.png'
 
 if not os.path.isfile(html_file):
-    print("ERROR:",html_file,"DOES NOT EXIST. Is your language code wrong or did you not build the HTML?")
+    print('ERROR:',html_file,'DOES NOT EXIST. Is your language code wrong or did you not build the HTML?')
     sys.exit(1)
 
 if not os.path.isfile(cabinet_file):
-    print("ERROR:",cabinet_file,"DOES NOT EXIST. Is your language code wrong or do you not have your spices?")
+    print('ERROR:',cabinet_file,'DOES NOT EXIST. Is your language code wrong or do you not have your spices?')
     sys.exit(1)
 
 #
@@ -82,18 +82,18 @@ header.append(menu)
 
 # Create toggle buttons, header, and nest
 toggles = soup.new_tag('div', **{'class':'togglebuttons'})
-menu = soup.new_tag(
+theme_toggle = soup.new_tag(
     'button',
     id='theme-toggle',
     string='☀',
 )
-toggles.append(menu)
-menu = soup.new_tag(
+toggles.append(theme_toggle)
+font_toggle = soup.new_tag(
     'button',
     id='font-toggle',
     string='Aa',
 )
-toggles.append(menu)
+toggles.append(font_toggle)
 
 # Extract table of contents navigation
 toc = soup.body.find('div', class_='ltx_page_main').nav.extract()
@@ -102,23 +102,23 @@ toc = soup.body.find('div', class_='ltx_page_main').nav.extract()
 toc['id'] = 'sidebar'
 
 # correcting a bug in latexml that drops em dash prefixes on toc in html output
-toc_emdashes = toc.select('a[href*="SSx"] > span')
+toc_emdashes = toc.select('a[href*=SSx] > span')
 for line in toc_emdashes:
     line.string.insert_before('—')
 
 # adding a toast
-# localization position 0: "Link Copied!"
-toast = soup.new_tag (
-    "div",
-    id="snackbar",
+# localization position 0: 'Link Copied!'
+toast = soup.new_tag(
+    'div',
+    id='snackbar',
     string=cabinet[0]
 )
 
 # add return to ref button
-# localization position 1: "Return to previous position"
-ref = soup.new_tag (
-    "div",
-    id="return",
+# localization position 1: 'Return to previous position'
+ref = soup.new_tag(
+    'div',
+    id='return',
     title=cabinet[1]
 )
 
@@ -132,8 +132,8 @@ soup.find('html')['lang'] = language
 #
 
 # meta: (property, content)
-# localization position 2: "A Practical Guide To Feminizing HRT"
-# localization position 3: "The futile attempt yadda yadda"
+# localization position 2: 'A Practical Guide To Feminizing HRT'
+# localization position 3: 'The futile attempt yadda yadda'
 meta_headers = [
     ('og:title', cabinet[2]),
     ('og:type', 'website'),
@@ -141,15 +141,6 @@ meta_headers = [
     ('og:image', cover_tag),
     ('og:description', cabinet[3])
 ]
-
-head_meta = soup.new_tag(
-    'meta',
-    attrs={
-        'name': 'theme-color',
-        'content': '#FFFFFF'
-    }
-)
-soup.head.extend([head_meta,"\n"])
 
 # links: (rel, type, href)
 link_headers = [
@@ -159,20 +150,29 @@ link_headers = [
 
 for attribs in meta_headers:
     head_meta = soup.new_tag(
-    'meta',
-    property=attribs[0],
-    content=attribs[1],
+        'meta',
+        property=attribs[0],
+        content=attribs[1],
     )
-    soup.head.extend([head_meta,"\n"])
+    soup.head.extend([head_meta,'\n'])
 
 for attribs in link_headers:
     head_meta = soup.new_tag(
-    'link',
-    rel=attribs[0],
-    type=attribs[1],
-    href=attribs[2],
+        'link',
+        rel=attribs[0],
+        type=attribs[1],
+        href=attribs[2],
     )
-    soup.head.extend([head_meta,"\n"])
+    soup.head.extend([head_meta,'\n'])
+
+meta_theme_color = soup.new_tag(
+    'meta',
+    attrs={
+        'name': 'theme-color',
+        'content': '#FFFFFF'
+    }
+)
+soup.head.extend([meta_theme_color,'\n'])
 
 head_meta = soup.new_tag(
     'script',
@@ -180,34 +180,34 @@ head_meta = soup.new_tag(
     src='/pghrtjs.js',
     defer='true'
 )
-soup.head.extend([head_meta,"\n"])
+soup.head.extend([head_meta,'\n'])
 
 #
 # content manipulation and adjustment
 #
 
 for element in soup.find_all(attrs={'title': 'In A PRACTICAL GUIDE TO FEMINIZING HRT'}):
-    del element["title"]
+    del element['title']
 
 # find all the section and question headers then add a click to copy icon
-# localization position 4: "Click to copy"
-for element in soup.find_all(["h2", "h3"]):
+# localization position 4: 'Click to copy'
+for element in soup.find_all(['h2', 'h3']):
     #find the id of its section
     hash = element.parent['id']
 
     new_chain = soup.new_tag(
         'a',
         **{'class':'chain'},
-        href="#" + hash,
+        href='#' + hash,
         title=cabinet[4],
-        onclick="copyURI(event)",
-        string=' 🔗',
+        onclick='copyURI(event)',
+        string='🔗',
     )
     element.append(new_chain)
 
 # changing pdf link
 # hosted on github because that keeps the site lighter under cloudflare's 25mb limit
-soup.find(href="PDF_LINK")['href'] = "https://raw.githubusercontent.com/Juicysteak117/pghrt/refs/heads/main/pdfs/pghrt_" + language + ".pdf"
+soup.find(href='PDF_LINK')['href'] = 'https://raw.githubusercontent.com/Juicysteak117/pghrt/refs/heads/main/pdfs/pghrt_' + language + '.pdf'
 
 # appending asset links to source of images for the html
 # can't get latexml to play nice with graphicspath so this is easier
@@ -216,22 +216,23 @@ for element in soup.select('figure > img'):
     element['src'] = '/img/' + old_string
 
 # hardcoding the other asset links so it plays nice with the subdomain
-soup.find(href="LaTeXML.css")['href'] = "/LaTeXML.css"
-soup.find(href="ltx-article.css")['href'] = "/ltx-article.css"
+# (hardcoding was removed, forgot to remove the rest, oops)
+#soup.find(href='LaTeXML.css')['href'] = '/LaTeXML.css'
+#soup.find(href='ltx-article.css')['href'] = '/ltx-article.css'
 
 # insert language flag links
-lang_links = soup.find('p', string="LANGUAGE-CODE-DOT-PGHRT-DOT-DIY")
+lang_links = soup.find('p', string='LANGUAGE-CODE-DOT-PGHRT-DOT-DIY')
 lang_links.clear()
 first_flag = True
 for lc, flag in lang_flags.items():
-    url = "https://" + lc + "." + domain
+    url = 'https://' + lc + '.' + domain
     if lc == 'en':
-        url = "https://" + domain
+        url = 'https://' + domain
     new_flag = soup.new_tag(
         'a',
         **{'class':'ltx_ref ltx_href'},
         href=url,
-        title=flag,
+        #title=flag,
         string=flag,
     )
     if not first_flag:
@@ -245,9 +246,9 @@ for lc, flag in lang_flags.items():
 cute = soup.new_tag(
     'img',
     id='cute',
-    src="/img/pghrt_88x31.png"
+    src='/img/pghrt_88x31.png'
 )
-soup.find('section',id="Sx1").insert_before(cute)
+soup.find('section',id='Sx1').insert_before(cute)
 
 # replacing \DTMNow with the footer timestamp because there aren't latexml
 # bindings for the datetime2 package and i want it to look prettier
@@ -255,18 +256,18 @@ soup.find('section',id="Sx1").insert_before(cute)
 # in an issue about it though. eventually i can remove that line lol
 # i realize this doesn't localize but truly do i care???? no. come on now.
 dtm = soup.body.find('span', class_='ltx_ERROR undefined')
-dtm['class'] = 'undefined'
+del dtm['class']
 timestamp = soup.footer.div.contents[0]
-postmarked = timestamp.text.replace("Generated  on ", "Generated on ")
+postmarked = timestamp.text.replace('Generated  on ', 'Generated on ')
 timestamp.replace_with(postmarked)
-dtm.string = postmarked.replace("Generated on ", "").replace(" by ", "")
+dtm.string = postmarked.replace('Generated on ', '').replace(' by ', '')
 
 #
 # garnish and serve
 #
 
 # i'm at soup
-print("soup made for language: ",language)
+print('soup made for language: ',language)
 
 # Write the updated soup back out to the file
 with open(html_file, 'w', encoding='utf-8') as fout:
